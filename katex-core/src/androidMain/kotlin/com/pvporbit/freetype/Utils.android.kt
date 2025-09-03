7b9b46311f39f44b7b078a1ddc2f1704ab649885
ketal <k@ketal.icu>
2025-09-03 13:09:20 +0800

    feat(core): Simplified asset file path handling

--- katex-core/src/androidMain/kotlin/com/pvporbit/freetype/Utils.android.kt
+++ katex-core/src/androidMain/kotlin/com/pvporbit/freetype/Utils.android.kt
@@ -8,11 +8,7 @@ lateinit var appContext: Context
 actual fun readAssetFile(path: String): ByteArray {
     return try {
         val realPath = Res.getUri(path)
-        val relativePath = if (realPath.startsWith("file:///android_asset/")) {
-            realPath.removePrefix("file:///android_asset/")
-        } else {
-            realPath
-        }
+        val relativePath = realPath.removePrefix("file:///android_asset/")
         val assetManager = appContext.assets
         assetManager.open(relativePath).use { inputStream ->
             inputStream.readBytes()
